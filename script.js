6849586f037c543891fe50ba712871e0d06325fc
Elizabeth Alabi <elizabeth200079@gmail.com>
2023-05-16 05:31:23 +0100

    Conduct intial fetch

---
+++ script.js
@@ -0,0 +1,14 @@
+// In your JavaScript file, create a function which houses a fetch() request to the RESTCountries API and returns the response in JSON format
+
+const getCountries = async ()=> {
+    const countryResponse = await fetch ("https://restcountries.com/v3.1/all")
+    console.log(request);
+}
+    
+
+
+
+
+// Create a SetUp() function which calls your first function and populates a global variable with the output. Have this function be called on load of your webpage
+
+
