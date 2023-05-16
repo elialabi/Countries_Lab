dda970b7076085366ebbf6b00cacad7a7147e581
Elizabeth Alabi <elizabeth200079@gmail.com>
2023-05-16 06:44:35 +0100

    Create a SetUp() function which calls your first function and populates a global variable with the output.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // In your JavaScript file, create a function which houses a fetch() request to the RESTCountries API and returns the response in JSON format
 
+let countriesData;
+
 const getCountries = () => {
      fetch ("https://restcountries.com/v3.1/all")
      .then(response => response.json())
-     .then(data => console.log(data))
-     .catch(error => console.error(error));
+     .then(data => {countriesData = data;
+        console.log(countriesata)
+     
+})
 }
 
 getCountries();
+
 
     
 
@@ -17,3 +22,8 @@ getCountries();
 // Create a SetUp() function which calls your first function and populates a global variable with the output. Have this function be called on load of your webpage
 
 
+const setUp = () => {
+    getCountries();
+}
+
+window.addEventListener("load", setUp); //window due to DOM
