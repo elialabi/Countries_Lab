6df870f6e28c98b48b4166d6d5a4e77401c7daa0
Elizabeth Alabi <elizabeth200079@gmail.com>
2023-05-16 09:08:38 +0100

    uploading

--- script.js
+++ script.js
@@ -6,17 +6,50 @@ const getCountries = () => {
      fetch ("https://restcountries.com/v3.1/all")
      .then(response => response.json())
      .then(data => {countriesData = data;
-        console.log(countriesata)
+        console.log(countriesData)
      
-})
+});
+
 }
 
-getCountries();
 
 
+const displayCountries = (data) => {
+    const listContainer = document.createElement("ul");
+
+    data.forEach((country) => {
+        
+        const countryItem = document.createElement("li");
+        const countryName = document.createElement("h2");
+        countryName.innerText = country.name.common;
+        const population = document.createElement("p");
+        population.innerText = `Population: ${country.population}`;
+        
+        countryItem.appendChild(countryName);
+        countryItem.appendChild(population);
+        listContainer.appendChild(countryItem);
     
+    });
 
+            //clear content b4 population 
 
+    const existingListcontainer = document.querySelector("#countriesList")
+    if(existingListcontainer) {
+        existingListcontainer.remove();
+    }
+
+    document.querySelector("#countriesContainer").appendChild(listContainer);
+
+}
+
+const filterCountries = (value) => {
+    const filteredData = countriesData.filter(country => {
+        const countryName = country.name.common.toLowerCase();
+        return countryName.includes(value.toLowerCase());
+    });
+
+    displayCountries(filteredData);
+}
 
 
 // Create a SetUp() function which calls your first function and populates a global variable with the output. Have this function be called on load of your webpage
@@ -24,6 +57,17 @@ getCountries();
 
 const setUp = () => {
     getCountries();
+
+    const form = document.querySelector("form");
+    const input = document.querySelector("input[type='text']");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log(input.value);
+        filterCountries(input.value);
+    });
+
 }
 
 window.addEventListener("load", setUp); //window due to DOM
+
